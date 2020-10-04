ac404e0fdeac5d2e4e43b96d8c5e631aa63c6a97
MartinKu <garcherai123@gmail.com>
2020-10-04 16:29:59 +0800

    property done

--- js/global.js
+++ js/global.js
@@ -210,7 +210,7 @@ barba.init({
 
       console.log(data.next.url);
 
-//      if(data.next.url.path === "propertyManagement.html"){
+
         var scriptt = document.createElement('script');
       scriptt.src = './js/property.js';
       data.next.container.appendChild(scriptt);
@@ -220,7 +220,7 @@ barba.init({
       linkt.href = 'style.css';
       data.next.container.appendChild(linkt);
       console.log(linkt);
-//      }
+
       
 
 
