4a8ef262d4cb15ebd633cb7d0e5fd03930f07b54
MartinKu <garcherai123@gmail.com>
2020-10-08 02:12:47 +0800

    fix json bug

--- js/hseDetail.js
+++ js/hseDetail.js
@@ -1,4 +1,8 @@
 $(function () {
+  var proID = 0;
+  $.getJSON("hseInfo.json", function (data) {
+    $("#house").html(data[proID].floorPlan);$("#i1").attr("src",data[proID].img1);
+    
+  });
 
-  
 })
