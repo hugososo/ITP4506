3be5ec20150fadd3dafe92dfb68e33de65419892
Hugo So <sohugo2008@gmail.com>
2020-10-11 00:17:59 +0800

    finish property search, remaining work is put js to global and make a role login redirect page

--- js/searchProperty.js
+++ js/searchProperty.js
@@ -16,11 +16,25 @@ $(document).ready(function () {
         $(".filter-container").slideToggle();
     });
 
-    let ac;
-
     $.getJSON('hseInfo.json', function (data) {
         property = data;
         let count = 0;
+
+        var mymap = L.map('map-container').setView([22.318455, 114.191612], 17);
+
+        L.tileLayer('https://mapapi.geodata.gov.hk/gs/api/v1.0.0/xyz/basemap/wgs84/{z}/{x}/{y}.png', {
+            attribution: '<u href="https://api.portal.hkmapservice.gov.hk/disclaimer" target="_blank" class="copyrightDiv">&copy; The Government of the Hong Kong SAR</u><div style="width:28px;height:28px;display:inline-flex;background:url(https://api.hkmapservice.gov.hk/mapapi/landsdlogo.jpg);background-size:28px;"></div>',
+            maxZoom: 17,
+            id: 'APIKEY'
+        }).addTo(mymap);
+
+        L.tileLayer('https://mapapi.geodata.gov.hk/gs/api/v1.0.0/xyz/label/hk/en/wgs84/{z}/{x}/{y}.png', {
+            maxZoom: 17,
+            id: 'APIKEY'
+        }).addTo(mymap);
+
+        var markers = L.markerClusterGroup();
+
         property.forEach(function (item, index, array) {
             count++;
             $("#property-result").append(
@@ -39,7 +53,34 @@ $(document).ready(function () {
                 '</div>'
             );
             $("#total-result").html(count);
+            markers.addLayer(L.marker(item.property_geo).bindPopup("<h2>" + item.name + "</h2><p>" + item.address + "</p>").addTo(mymap));
         });
+        markers.on('click', function (a) {
+            count=0;
+            const geoData = Object.values(a.layer._latlng);
+            property.filter(function (item, index, array) {
+                if (JSON.stringify(item.property_geo) == JSON.stringify(geoData)) {
+                    count++;
+                    $("#property-result").html(
+                        '<div class="user-card">' +
+                        '<div class="user-card-img">' +
+                        '<img src="' + item.img1 + '" alt="" width="150px" height="100px">' +
+                        '</div>' +
+                        '<div class="user-card-content" style="margin-left:10px">' +
+                        '<h3 class="user-card-text-1">' + item.name + '</h3>' +
+                        '<p class="user-card-text-2">' + item.address + '</p>' +
+                        '<p style="color: rgb(70, 70, 70);">' + item.numOfRoom + " Room | " + item.type + '</p>' +
+                        '<span class="user-card-text-3" style="color:rgb(255, 50, 93);font-size:20pt">$' + item.price + '</span>' +
+                        '<span style="margin-left:5px;font-size:12pt">' + item.size + " sq.ft." + '</span>' +
+                        '</div>' +
+                        '<a style="text-decoration:none;position:absolute;width:100%;height:100%;z-index:1;" href="houseDetail.html?index=' + index + '"/>' +
+                        '</div>'
+                    );
+                    $("#total-result").html(count);
+                }
+            });
+        });
+        mymap.addLayer(markers);
     });
 
     $('.addfilter input:checkbox,input:radio').prop("checked", false);
@@ -70,6 +111,7 @@ $(document).ready(function () {
     });
 
     $('.resetbtn').click(function () {
+        count = 0;
         $(".belongitem1").appendTo("#item-container-1");
         $(".belongitem2").appendTo("#item-container-2");
         $(".belongitem3").appendTo("#item-container-3");
@@ -78,7 +120,29 @@ $(document).ready(function () {
         $(".belongitem6").appendTo("#item-container-6");
         $('.addfilter input:checkbox,input:radio').prop("checked", false);
         $('.searchbar').val("");
-        $(".searchbtn").click();
+        $.getJSON('hseInfo.json', function (data) {
+            $("#property-result").html("");
+            property = data;
+            property.forEach(function (item, index, array) {
+                count++;
+                $("#property-result").append(
+                    '<div class="user-card">' +
+                    '<div class="user-card-img">' +
+                    '<img src="' + item.img1 + '" alt="" width="150px" height="100px">' +
+                    '</div>' +
+                    '<div class="user-card-content" style="margin-left:10px">' +
+                    '<h3 class="user-card-text-1">' + item.name + '</h3>' +
+                    '<p class="user-card-text-2">' + item.address + '</p>' +
+                    '<p style="color: rgb(70, 70, 70);">' + item.numOfRoom + " Room | " + item.type + '</p>' +
+                    '<span class="user-card-text-3" style="color:rgb(255, 50, 93);font-size:20pt">$' + item.price + '</span>' +
+                    '<span style="margin-left:5px;font-size:12pt">' + item.size + " sq.ft." + '</span>' +
+                    '</div>' +
+                    '<a style="text-decoration:none;position:absolute;width:100%;height:100%;z-index:1;" href="houseDetail.html?index=' + index + '"/>' +
+                    '</div>'
+                );
+                $("#total-result").html(count);
+            });
+        });
     });
 
     $('#price-range-selector').click(function () {
@@ -90,57 +154,6 @@ $(document).ready(function () {
                 $(this).appendTo("#item-container-6");
         }
     });
-
-    var mymap = L.map('map-container').setView([22.318455, 114.191612], 17);
-
-    L.tileLayer('https://mapapi.geodata.gov.hk/gs/api/v1.0.0/xyz/basemap/wgs84/{z}/{x}/{y}.png', {
-        attribution: '<u href="https://api.portal.hkmapservice.gov.hk/disclaimer" target="_blank" class="copyrightDiv">&copy; The Government of the Hong Kong SAR</u><div style="width:28px;height:28px;display:inline-flex;background:url(https://api.hkmapservice.gov.hk/mapapi/landsdlogo.jpg);background-size:28px;"></div>',
-        maxZoom: 17,
-        id: 'APIKEY'
-    }).addTo(mymap);
-
-    L.tileLayer('https://mapapi.geodata.gov.hk/gs/api/v1.0.0/xyz/label/hk/en/wgs84/{z}/{x}/{y}.png', {
-        maxZoom: 17,
-        id: 'APIKEY'
-    }).addTo(mymap);
-    
-    const option = {
-        title:'tltle'
-    }
-
-    var markers = L.markerClusterGroup({
-        singleMarkerMode:true
-    });
-    markers.addLayer(L.marker([22.318455, 114.191612]).bindPopup("<h2>Wyler Gardens</h2><p>To Kwa Wan Rd 125 Kowloon</p>").addTo(mymap).on('click',onClick));
-    markers.addLayer(L.marker([22.311500, 114.191812]).bindPopup("<h2>HONG WAH COURT</h2><p>To Kwa Wan Rd 128 Kowloon</p>").addTo(mymap));
-    markers.addLayer(L.marker([22.317500, 114.191812]).bindPopup("<h2>HONG WAH COURT</h2><p>To Kwa Wan Rd 128 Kowloon</p>").addTo(mymap));
-    markers.addLayer(L.marker([22.323500, 114.191812]).bindPopup("<h2>HONG WAH COURT</h2><p>To Kwa Wan Rd 128 Kowloon</p>").addTo(mymap));
-    markers.addLayer(L.marker([22.317500, 114.141812]).bindPopup("<h2>HONG WAH COURT</h2><p>To Kwa Wan Rd 128 Kowloon</p>").addTo(mymap));
-    markers.addLayer(L.marker([22.312500, 114.111812]).bindPopup("<h2>HONG WAH COURT</h2><p>To Kwa Wan Rd 128 Kowloon</p>").addTo(mymap));
-    markers.addLayer(L.marker([22.317500, 114.291812]).bindPopup("<h2>HONG WAH COURT</h2><p>To Kwa Wan Rd 128 Kowloon</p>").addTo(mymap));
-    markers.addLayer(L.marker([22.317500, 114.101812]).bindPopup("<h2>HONG WAH COURT</h2><p>To Kwa Wan Rd 128 Kowloon</p>").addTo(mymap));
-    markers.addLayer(L.marker([22.417500, 114.241812]).bindPopup("<h2>HONG WAH COURT</h2><p>To Kwa Wan Rd 128 Kowloon</p>").addTo(mymap));
-    markers.addLayer(L.marker([22.357500, 114.151812]).bindPopup("<h2>HONG WAH COURT</h2><p>To Kwa Wan Rd 128 Kowloon</p>").addTo(mymap));
-    markers.addLayer(L.marker([22.327500, 114.121812]).bindPopup("<h2>HONG WAH COURT</h2><p>To Kwa Wan Rd 128 Kowloon</p>").addTo(mymap));
-    mymap.addLayer(markers);
-
-    function onClick(e){
-        $("#property-result").html(
-            '<div class="user-card">' +
-            '<div class="user-card-img">' +
-            '<img src="./assets/hseInfo/WYLER_GARDENS_1.jpg" alt="" width="150px" height="100px">' +
-            '</div>' +
-            '<div class="user-card-content" style="margin-left:10px">' +
-            '<h3 class="user-card-text-1">WYLER GARDENS Block C</h3>' +
-            '<p class="user-card-text-2">To Kwa Wan Rd 125 Kowloon</p>' +
-            '<p style="color: rgb(70, 70, 70);">2 Room | Private housing estates</p>' +
-            '<span class="user-card-text-3" style="color:rgb(255, 50, 93);font-size:20pt">$6.88M</span>' +
-            '<span style="margin-left:5px;font-size:12pt">456 sq.ft.</span>' +
-            '</div>' +
-            '<a style="text-decoration:none;position:absolute;width:100%;height:100%;z-index:1;" href="houseDetail.html?index=0"/>' +
-            '</div>'
-        );
-    }
 
     // $(".searchbtn").click(function () {
     //     $(".userresult-container").html("");
